b2d006086b6d09b44aeb32f3984ec2a477c796d6
anhduc0523 <anhducntt2002@gmail.com>
2025-05-07 10:45:33 +0700

    feat: add Swagger UI configuration and CORS support for API services

--- api-gateway/src/main/java/com/anhduc0523/microservices/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/anhduc0523/microservices/gateway/routes/Routes.java
@@ -8,12 +8,22 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute(){
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("/api/v1/products"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> publicServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v1/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
@@ -25,9 +35,25 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v1/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("/api/v1/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v1/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 }
--- order-service/src/main/java/com/anhduc0523/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/anhduc0523/microservices/order/client/InventoryClient.java
@@ -1,12 +1,9 @@
 package com.anhduc0523.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.service.url}")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/inventory")
+    @GetExchange("api/v1/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
